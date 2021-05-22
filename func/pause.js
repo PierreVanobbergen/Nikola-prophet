f9b7d02b30cd8f62c36609ce367481123de93e4f
Pierre Vanobbergen <pierrevob@hotmail.com>
2021-05-22 16:14:18 +0200

    Remove emotes

--- func/pause.js
+++ func/pause.js
@@ -4,16 +4,16 @@ module.exports = {
 
     execute(client, message) {
         if (!message.member.voice.channel)
-            return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
+            return message.channel.send(`- You're not in a voice channel !`);
 
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id)
-            return message.channel.send(`${client.emotes.error} - You are not in the same voice channel !`);
+            return message.channel.send(`- You are not in the same voice channel !`);
 
         if (!client.player.getQueue(message))
-            return message.channel.send(`${client.emotes.error} - No music currently playing !`);
+            return message.channel.send(`- No music currently playing !`);
 
         if (client.player.getQueue(message).paused)
-            return message.channel.send(`${client.emotes.error} - The music is already paused !`);
+            return message.channel.send(`- The music is already paused !`);
 
         const success = client.player.pause(message);
 
